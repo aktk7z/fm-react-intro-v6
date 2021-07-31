533a0f9c750257ed843166ab90d9fdd3a1972f33
x1a09i <x1a09i.dev@gmail.com>
2021-08-01 05:26:32 +0900

    Add useBreedList custom hook

---
+++ src/useBreedList.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const localCache = {};
+
+export const useBreedList = (animal) => {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    console.log(localCache);
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    // can not use arrow functions here
+    async function requestBreedList() {
+      // make sure there are no wired interim states
+      setBreedList([]);
+      setStatus("loading");
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+};
