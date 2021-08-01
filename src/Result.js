774a6c6340f2fa56561fc3d37b111cee0cbc211c
x1a09i <x1a09i.dev@gmail.com>
2021-08-01 12:49:50 +0900

    extract Pets from SearchParams to single Result page

---
+++ src/Result.js
@@ -0,0 +1,27 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No Pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            id={pet.id}
+            key={pet.id}
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+          />
+          // A BAD idea
+          //   <Pet {...pet} key={pet.id} />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Pet from "./Pet";
+import Results from "./Result";
 import { useBreedList } from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -77,9 +77,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet name={pet.name} animal={pet.animal} key={pet.id} />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
