511c4f40a806cf043958301acee27b32f542da6d
x1a09i <x1a09i.dev@gmail.com>
2021-08-09 23:23:07 +0900

    Add Detalis page(Class Component)

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,42 @@
-const Details = () => {
-  return <h2>hi lolol omg wtf!</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router";
 
-export default Details;
+class Details extends Component {
+  constructor() {
+    super();
+
+    // Default State
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      // Lazy Brain writting lazy code :D
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    console.log(this.state);
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${city} - ${state}`}</h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
